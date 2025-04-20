4b2d210ec362720f92d464ffd23577095ed746bc
Luke Jones <101lukej@gmail.com>
2025-04-20 20:45:02 +1200

    first commit

---
+++ course-tracker-sketch.ino
@@ -0,0 +1,17 @@
+#include <> // Library for LCD
+
+int CATAGORY_SIZE = 5
+
+string CATAGORIES[CATAGORY_SIZE] = {'ENEL200', 'ENEL220', 'ENEL270', 'ENEL280', 'EMTH210'}
+int cat_times[CATAGORY_SIZE] = {0, 0, 0, 0, 0}
+
+
+void setup() {
+  // put  your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
